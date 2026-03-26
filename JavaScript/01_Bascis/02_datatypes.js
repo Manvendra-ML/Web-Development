dc42ccd8600171c732d5c57fedb1482ac6e5d77a
Manvendra <manvendrajhansi1406@gmail.com>
2026-03-26 18:44:07 +0000

    data types in js

--- JavaScript/01_Bascis/02_datatypes.js
+++ JavaScript/01_Bascis/02_datatypes.js
@@ -1,3 +1,15 @@
 "use strict" // use only new version of javascript.
 
 alert(3+3) // it is not work here because we run our code in node.
+
+// Premetive Data Type ( these data type are immutable)
+
+let name = "username " // It is a string datatype
+let age = 19 //Number data type
+let something = true // boolean data type
+
+// Important 
+let temp = null // it is s special data type which indicates null value of variable in javascript
+
+let college;
+// if variable is not assign with any value then its data type is "undefined"
