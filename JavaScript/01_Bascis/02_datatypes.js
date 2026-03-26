d340420e9fbf751e55b72c3e5cfec2d0d338902f
Manvendra <manvendrajhansi1406@gmail.com>
2026-03-26 18:50:33 +0000

    non-premetive dt

--- JavaScript/01_Bascis/02_datatypes.js
+++ JavaScript/01_Bascis/02_datatypes.js
@@ -1,6 +1,6 @@
 "use strict" // use only new version of javascript.
 
-alert(3+3) // it is not work here because we run our code in node.
+//alert(3+3) // it is not work here because we run our code in node.
 
 // Premetive Data Type ( these data type are immutable)
 
@@ -13,3 +13,11 @@ let temp = null // it is s special data type which indicates null value of varia
 
 let college;
 // if variable is not assign with any value then its data type is "undefined"
+
+let sym = Symbol("manvendra") // Symbol use to assign unique value.
+
+// Non - premetive 
+
+// Array, object and functions.
+
+console.log(typeof sym); // give the data type of given variable or value.
