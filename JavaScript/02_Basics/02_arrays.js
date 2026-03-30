a9879b48166f7faa7840e87def27ba6cc9ccfe43
Manvendra <manvendrajhansi1406@gmail.com>
2026-03-30 19:13:06 +0000

    flat function

--- JavaScript/02_Basics/02_arrays.js
+++ JavaScript/02_Basics/02_arrays.js
@@ -8,11 +8,18 @@ const nits = ["NIT Warangal", "NIT Trichy", "NIT Allahbad"]
 
 // to avoid this we use concat function
 
-const colleges = iits.concat(nits) // it merge all elements in both arrays and return a new array.
+// const colleges = iits.concat(nits) // it merge all elements in both arrays and return a new array.
 
-// console.log(colleges);
+// // console.log(colleges);
 
-// If there are more than two arrays then we use the concept of spread.
+// // If there are more than two arrays then we use the concept of spread.
 
-const new_array = [...iits, ...nits, ...colleges] // we can use wanted number of arrays to merge
-console.log(new_array);
+// const new_array = [...iits, ...nits, ...colleges] // we can use wanted number of arrays to merge
+// console.log(new_array);
+
+const mixed_array = [1,2,3, [5,4], "string", true, ["mohan", [1,2], "ritesh"]]
+
+// if we encounter with something like this we use flat function
+
+const fixed_array = mixed_array.flat(Infinity) // infinity is the level to check (here we needed only 2 level)
+console.log(fixed_array);
