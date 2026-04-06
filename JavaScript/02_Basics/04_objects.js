9413d224cc89defedfe37558be9a7ebe3e86ee5c
Manvendra <manvendrajhansi1406@gmail.com>
2026-04-06 17:52:11 +0000

    singleton object

---
+++ JavaScript/02_Basics/04_objects.js
@@ -0,0 +1,53 @@
+// Singelton Objects
+
+const app_user = new Object(); // this is called singleton object.
+
+// we can assign values to it 
+
+app_user.id = "125newId";
+app_user.name = "Username";
+app_user.isLoggedIn = false;
+
+console.log(app_user);
+
+// We can also do nesting in objects
+
+const new_user = {
+    email : "some@gmail.com",
+    fullname : {
+        firstName : "Jaskrit",
+        middleName : "Singh",
+        lastName : "Ranghi"
+    }
+}
+console.log(new_user.fullname);
+console.log(typeof new_user.fullname); // As expected, it shows object.
+console.log(new_user.fullname.firstName); // we can access the values as an object.
+
+// Concatinate two or more objects
+
+const ob1 = {0 : 'a', 1 : 'b'}
+const ob2 = {3 : 'a', 5 : 'b'}
+const ob3 = {4 : 'a', 6 : 'b'}
+
+const new_ob = Object.assign(ob1, ob2) // here ob1 is target (which mean all value of ob2 assign in ob1) and ob2 is sorce.
+// no. of source can be increase but there is only one target (which is first objects).
+// To avoid changes in first objects,we pass an empty object as target
+const new_ob2= Object.assign({}, ob2, ob3)
+
+console.log(new_ob);
+console.log(ob1);
+
+console.log(new_ob2);
+
+console.log(app_user);
+
+console.log(Object.keys(app_user)); // display only keys(data type : array).
+console.log(Object.values(app_user)); // display only values(data type : array).
+console.log(Object.entries(app_user)); // Entry is a pair of key and value(data type : array).
+
+// It is also a important method
+console.log(app_user .hasOwnProperty(name)); // return boolean type data for asked question.
+
+
+
